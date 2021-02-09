d72d83c3dfb43f3a24a653e78e16f417d003f30d
Roi Yashar <roiysh@gmail.com>
2021-02-09 02:15:16 +0200

    use databinding from mockdata

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,29 @@
-import { Component } from '@angular/core';
-
+import { Component , EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Product} from '../app/Product';
+import {ProductsStoreData} from '../app/mock-Products';
+import { stringify } from '@angular/compiler/src/util';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   //template:{<h1>sfdf</h1>},
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Tour of Heros';
-  // onClick():void{
-  //    this.title = this.title?'': 'Tour of Heros';
-  //   console.log("Display");
-  // }
+export class AppComponent implements OnInit {
+  title = 'My Shop';
+  selectedProduct?:Product;
+  ProductsData:Product[] =ProductsStoreData;
+  categoryList?:string[] ;
+  constructor() {
+    this.categoryList= undefined;//_.uniqBy(this.ProductsData, 'category');
+  }
+  ngOnInit(): void {}
+   onSelect(product:Product):void{
+    if(this.selectedProduct==product){
+      this.selectedProduct = undefined;
+      console.log("onSelect:" + JSON.stringify(product));
+    }
+    else
+      this.selectedProduct = product;
+     
+   }  
 }
